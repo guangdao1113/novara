004533c6383c8c1c8d029cafa73973529e23e8ca
Penny520520 <82118825+Penny520520@users.noreply.github.com>
2024-01-02 21:19:16 -0800

    update-final-2
    update-final-2

--- components/subscribeEmail.tsx
+++ components/subscribeEmail.tsx
@@ -160,7 +160,7 @@ function CustomForm({
   );
 }
 export default function SubscribeEmail(): JSX.Element {
-  const postUrl = `${process.env.PENNY_PUBLIC_MAILCHIMP_ACTION_URL}?u=${process.env.PENNY_PUBLIC_MAILCHIMP_U_VALUE}&id=${process.env.PENNY_PUBLIC_MAILCHIMP_ID_VALUE}`;
+  const postUrl = `${process.env.NEXT_PUBLIC_MAILCHIMP_ACTION_URL}?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U_VALUE}&id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID_VALUE}`;
   //const postUrl = `https://yahoo.us13.list-manage.com/subscribe/post?u=56a5a6faefa46b019dbd969e7&id=c250b296eb`;
   return (
     <div className={styles.cusFormContainer}>
